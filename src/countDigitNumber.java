9b4a3d07ce5a5c4aa1c3d83ddf2fa93f4ceb6ac7
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-01-24 21:06:35 +0530

    first commit

---
+++ src/countDigitNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class countDigitNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number :");
+        int n=sc.nextInt();
+        int cout=0;
+        while(n!=0){
+            n=n/10;
+            cout++;
+        }
+        System.out.println("the count of digits of given number is "+cout);
+
+    }
+}
