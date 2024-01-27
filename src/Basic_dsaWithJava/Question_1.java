bb20a0fd0123ef0cb94e926ff8fc5298e276ac8a
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-01-27 21:51:31 +0530

    first commit

---
+++ src/Basic_dsaWithJava/Question_1.java
@@ -0,0 +1,7 @@
+package Basic_dsaWithJava;
+
+public class Question_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello,world \n Java is great ");
+    }
+}
---
+++ src/sumOfDigitsOfGivenNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sumOfDigitsOfGivenNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a number :");
+        int n=sc.nextInt();
+        int sum=0;
+        int x;
+        while(n!=0){
+            x=n%10;
+            sum=sum+x;
+            n=n/10;
+
+
+        }
+        System.out.println("sum of digits of a given number is "+sum);
+    }
+}
