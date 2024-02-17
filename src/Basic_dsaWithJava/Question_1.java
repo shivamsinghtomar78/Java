d8a0a808de43e70e0ada978673d2f9331cc7bfc1
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-02-17 19:59:44 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/Basic_dsaWithJava/Question_1.java
+++ src/Basic_dsaWithJava/Question_1.java
@@ -2,6 +2,9 @@ package Basic_dsaWithJava;
 
 public class Question_1 {
     public static void main(String[] args) {
-        System.out.println("Hello,world \n Java is great ");
+        //Q.Explain the purpose of the escape sequence \n in Java. Provide an example of its usage in a program,
+        //and predict the output.
+        System.out.println("Hello,World\nJava is great");
     }
 }
+
