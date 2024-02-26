009cf03bec0ed3a32c28f28c62bb025a25dd667b
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-02-26 18:32:36 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	src/pattern_Printing/starRectange.java

--- src/pattern_Printing/starTriangle.java
+++ src/pattern_Printing/starTriangle.java
@@ -2,7 +2,6 @@ package pattern_Printing;
 
 public class starTriangle {
     public static void main(String[] args) {
-
         for (int i=0;i<4;i++){
 
             for (int j = 0; j <=i ; j++) {
@@ -13,3 +12,4 @@ public class starTriangle {
         }
     }
 }
+
