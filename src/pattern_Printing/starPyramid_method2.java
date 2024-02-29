807b59b20590c2f19c1688d280aa3a0fcdd1a16d
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-02-29 12:00:17 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	src/pattern_Printing/starRectange.java

---
+++ src/pattern_Printing/starPyramid_method2.java
@@ -0,0 +1,30 @@
+package pattern_Printing;
+
+import java.util.Scanner;
+
+public class starPyramid_method2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+        int nsp=n-1;
+        int nst=1;
+        for (int i = 1; i <=n ; i++) {
+            for (int j = 1; j <=nsp ; j++) {
+                System.out.print(" "+" ");
+
+
+            }
+            for (int j = 1; j <=nst ; j++) {
+                System.out.print("*"+" ");
+
+            }
+            System.out.println();
+            nsp--;
+            nst+=2;
+
+
+        }
+
+    }
+}
