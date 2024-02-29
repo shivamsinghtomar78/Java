9a1beed00b68f6f009ad3b3f6d605afa7a6c313d
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-02-29 10:03:13 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml
    #	src/pattern_Printing/starRectange.java

---
+++ src/pattern_Printing/numberTriangleFlipped_method2.java
@@ -0,0 +1,23 @@
+package pattern_Printing;
+
+import java.util.Scanner;
+
+public class numberTriangleFlipped_method2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n:");
+        int n=sc.nextInt();
+
+        for (int i = 1; i <=n ; i++) {
+            int m=1;
+            for (int j =1; j <=n-i ; j++) {
+                System.out.print(" "+" ");
+            }
+            for (int j =1; j <=i ; j++) {
+                System.out.print(m+" ");
+                m=m+1;
+            }
+            System.out.println();
+        }
+    }
+}
