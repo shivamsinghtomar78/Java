2ce0d7fbb6eced6e03f252dbd8637d5e74c128fd
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-04-27 11:16:29 +0530

    new question

--- src/Recursion/permutation.java
+++ src/Recursion/permutation.java
@@ -1,7 +1,23 @@
 package Recursion;
 
 public class permutation {
+    public static void printPermutation(String ans,String s){
+        if(s.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        for (int i = 0; i <s.length() ; i++) {
+            char ch=s.charAt(i);
+            String left=s.substring(0,i);
+            String right=s.substring(i+1);
+            printPermutation(ans+ch,left+right);
+
+
+        }
+    }
     public static void main(String[] args) {
+        String s="abcd";
+        printPermutation("",s);
 
     }
 }
