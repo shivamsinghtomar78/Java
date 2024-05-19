87b8bbd1d1ae0e90cb0527f07b0b77df1ae60b28
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-05-19 19:37:19 +0530

    new question

--- src/linkedList/ListNodeClass.java
+++ src/linkedList/ListNodeClass.java
@@ -10,14 +10,14 @@ public class ListNodeClass {
     public static void print(Node head){
         Node temp=head;
         while(temp!=null) {
-            System.out.println(temp.val);
+            System.out.print(temp.val+" ");
             temp = temp.next;
         }
-
+        System.out.println();
     }
     public static void displayRecursive(Node head){
          if(head==null) return ;// base case
-        System.out.println(head.val);
+        System.out.print(head.val+" ");
         displayRecursive(head.next);
     }
 //public static void displayRecursive(Node head){// this print reverse form
@@ -27,17 +27,17 @@ public class ListNodeClass {
 //    System.out.print(head.val+" ");
 //}
     public static void main(String[] args) {
-        Node a=new Node(15);
-        Node b=new Node(25);
-        Node c=new Node(35);
-        Node d=new Node(45);
-        Node e=new Node(55);
-        Node f=new Node(65);
+        Node a=new Node(10);
+        Node b=new Node(20);
+        Node c=new Node(30);
+        Node d=new Node(40);
+        Node e=new Node(50);
+        Node f=new Node(60);
         a.next=b;// linking => a->b  => 10->20
         b.next=c;// linking => b->c  => 10->20->30
         c.next=d;// linking => c->d  => 10->20->30->40
         d.next=e;// linking => d->e  => 10->20->30->40->50
-        e.next=f;
+        e.next=f;// linking => e->f  => 10->20->30->40->50->65
         print(a);
         displayRecursive(a);
 //        System.out.println(a.val);
