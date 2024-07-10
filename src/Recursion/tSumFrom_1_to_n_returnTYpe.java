1d894cda54204b271ca77f2ce1f29d8bbfefadb4
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-10 11:53:19 +0530

    new question

--- src/Recursion/tSumFrom_1_to_n_returnTYpe.java
+++ src/Recursion/tSumFrom_1_to_n_returnTYpe.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class tSumFrom_1_to_n_returnTYpe {
     public static int sum(int n){
-   if(n==1 || n==0) return 1;
+   if(n==1) return 1;
    return n+sum(n-1);
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("enter n:");
         int n=sc.nextInt();
-        System.out.println( sum(n));
+        System.out.println("sum: "+ sum(n));
     }
 }
