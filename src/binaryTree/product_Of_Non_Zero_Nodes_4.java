40caf27dcf1eaf9cf19044f98c4bd83c6e51d3c9
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-11 09:43:50 +0530

    new question

--- src/binaryTree/product_Of_Non_Zero_Nodes_4.java
+++ src/binaryTree/product_Of_Non_Zero_Nodes_4.java
@@ -1,14 +1,14 @@
 package binaryTree;
 
-public class productOfTreeNodes_3 {
+public class product_Of_Non_Zero_Nodes_4 {
     public static void main(String[] args) {
         Node a=new Node(1);// a is the root
-        Node b=new Node(4);
+        Node b=new Node(0);
         Node c=new Node(3);
-        Node d=new Node(2);
+        Node d=new Node(0);
         Node e=new Node(6);
         Node f=new Node(5);
-        Node g=new Node(10);
+        Node g=new Node(0);
         Node h=new Node(20);
         // connecting
         a.left=b; a.right=c;
@@ -16,12 +16,15 @@ public class productOfTreeNodes_3 {
         c.right=f; c.left=g;
         e.right=h;
         int ans=product(a);
-        System.out.println("Product: "+ans);
+        System.out.println("Product of Non-Zero Nodes: "+ans);
     }
     private static int product(Node root){
         if(root==null) return 1;
-        int a=product(root.left);
-        int b=product(root.right);
+        int a= product(root.left);
+        int b= product(root.right);
+        if (root.val == 0) {
+            return a * b;
+        }
         return root.val*a*b;
     }
 }
