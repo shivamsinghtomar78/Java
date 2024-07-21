eb01cc94c483844dacfc70311a8304f424840a0b
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-21 22:35:53 +0530

    new commit

--- src/challenge_/book.java
+++ src/challenge_/book.java
@@ -1,4 +1,66 @@
 package challenge_;
 
-public class book {
+
+
+  class Book {
+      static int totalNoOfBooks;
+      String author ;
+      String title;
+      int isbn;
+      boolean isBorrowed;
+      static{
+          totalNoOfBooks=0;
+      }
+
+      {// Object init
+          totalNoOfBooks++;
+      }
+
+      Book(int  isbn,String title,String author){
+          this.title=title;
+          this.isbn=isbn;
+          this.author=author;
+      }
+      Book(int  isbn){
+          this(isbn,"Unknown","Unknown");
+      }
+      static int getTotalNoOfBooks(){
+          return totalNoOfBooks;
+      }
+      void borrowBook(){
+          if(isBorrowed) {
+              System.out.println("Book is already borrowed !!");
+          }
+         else{
+             this.isBorrowed=true;
+              System.out.println("Enjoy "+this.title);
+          }
+      }
+      void returnBook(){
+          if(isBorrowed){
+              this.isBorrowed=false;
+              System.out.println("Hope you enjoyed ,please leave a review");
+          }
+          else{
+              System.out.println("This book is already in the library!");
+          }
+      }
+
+      public static void main(String[] args) {
+          Book designOfThings =new Book(1,"design","Author");
+          Book myBook =new Book(2);
+          System.out.println(Book.getTotalNoOfBooks());
+          designOfThings.borrowBook();
+          designOfThings.borrowBook();
+          designOfThings.returnBook();
+          designOfThings.returnBook();
+          myBook.returnBook();
+          designOfThings.borrowBook();
+
+
+
+      }
+
+
+
 }
