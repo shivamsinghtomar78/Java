ca65cd89c94050406dc9904a39c3ca8c04531561
shivamsinghtomar78 <139133103+shivamsinghtomar78@users.noreply.github.com>
2024-07-26 12:34:34 +0530

    new commit

--- src/array/practiceQ9_method_2.java
+++ src/array/practiceQ9_method_2.java
@@ -2,19 +2,21 @@ package array;
 
 public class practiceQ9_method_2 {
     public static void main(String[] args) {
-        //Q.sort the array of 0's and 1's.
-        int []arr={1,0,0,1,1,0,0,0,1,0};
-        int n=arr.length;
-        //method-2-> one pass soln
-        int i=0,j=n-1;
-        while(i<j) {
+        // Q.sort the array of 0's and 1's.
+        int[] arr = {1,0,0,1,1,0,0,0,1,0};
+        int n = arr.length;
+
+        // method-2 -> one pass solution
+        int i = 0, j = n - 1;
+        while (i < j) {
             if (arr[i] == 0) {
                 i++;
+                continue;
             }
-            if (arr[j] == 0) {
+            if (arr[j] == 1) {
                 j--;
+                continue;
             }
-            if(i>j) break;
             if (arr[i] == 1 && arr[j] == 0) {
                 arr[i] = 0;
                 arr[j] = 1;
@@ -22,15 +24,10 @@ public class practiceQ9_method_2 {
                 j--;
             }
         }
-        for (int ele:arr) {
-            System.out.print(ele+" ");
 
-
+        for (int ele : arr) {
+            System.out.print(ele + " ");
         }
-
-
-
-        }
-
     }
+}
 
